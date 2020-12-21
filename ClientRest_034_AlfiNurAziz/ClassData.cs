275fb1065a9700735e1a90e0782d9f7e54290033
Aalfinuraziz <alfinuraziz885@gmail.com>
2020-12-21 22:53:47 +0700

    update

--- ClientRest_034_AlfiNurAziz/ClassData.cs
+++ ClientRest_034_AlfiNurAziz/ClassData.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 using System.Runtime.Serialization;
 using System.Net;
+using static ClientRest_034_AlfiNurAziz.Program;
 
 namespace ClientRest_034_AlfiNurAziz
 {
